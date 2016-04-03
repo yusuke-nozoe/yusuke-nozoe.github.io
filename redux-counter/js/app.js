9aa1794b89f2978e6710a554709b0f6efb253c19
Yusuke Nozoe <yusuke.nozoe@asbetcorp.com>
2016-04-03 17:57:52 +0900

    add redux counter

--- redux-counter/js/app.js
+++ redux-counter/js/app.js
@@ -1,7 +1,38 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import { createStore } from 'redux'
+
+// This is a reducer, a pure function with (state, action) => state signature.
+function counter(state = { count: 0 }, action) {
+  switch (action.type) {
+  case 'INCREMENT':
+    return {
+      count: state.count + 1
+    };
+  case 'DECREMENT':
+    return {
+      count: state.count - 1
+    };
+  default:
+    return state
+  }
+}
+
+// Create a Redux store holding the state of your app.
+// Its API is { subscribe, dispatch, getState }.
+let store = createStore(counter)
+
+// You can subscribe to the updates manually, or use bindings to your view layer.
+store.subscribe(() =>
+  console.log(store.getState())
+)
+
+import RootComponent from './components';
 
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <Provider store={store}>
+    <RootComponent />
+  </Provider>,
   document.getElementById('react-app')
 );
